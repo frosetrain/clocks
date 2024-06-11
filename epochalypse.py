17fcf17a084c41c16a976075da1802558e3ca59b
Frosetrain <80436189+frosetrain@users.noreply.github.com>
2024-06-11 15:46:44 +0800

    Add countdown to epochalypse

--- epochalypse.py
+++ epochalypse.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
 print()
+print()
 while True:
-    print(f"\033[F{datetime.now().timestamp() / 2 ** 31}")
+    print(f"\033[F\033[F{datetime.now().timestamp() / 2 ** 31}")
+    print(datetime.fromtimestamp(2 ** 31 * 0.8) - datetime.now())
